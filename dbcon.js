bab87d40cf72202b51e0e4cadd8d99102c1fe2ab
Nathan Zimmerman <natez56@hotmail.com>
2017-08-12 13:46:37 -0700

    added code to create tables

--- dbcon.js
+++ dbcon.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_xxxxx',
-  password        : 'xxxxxxx',
-  database        : 'cs340_xxxxx'
+  user            : 'cs340_******',
+  password        : '*****',
+  database        : 'cs340_*****'
 });
 
 module.exports.pool = pool;
--- forms.js
+++ forms.js
@@ -52,6 +52,114 @@ app.get('/movieProductionCo',function(req,res,next){
     res.render('movieProductionCo');
 });
 
+app.get('/create',function(req,res,next){
+  var context = {};
+
+  mysql.pool.query("DROP TABLE IF EXISTS `Movie`", function(err){ //replace your connection pool with the your variable containing the connection pool
+    var createString = "CREATE TABLE Movie("+
+    "movie_id INT AUTO_INCREMENT,"+
+    "title VARCHAR(255) NOT NULL,"+
+    "duration INT NOT NULL,"+
+    "release_year INT NOT NULL,"+
+    "PRIMARY KEY(movie_id));";
+    mysql.pool.query(createString, function(err){
+      context.results = "Table reset";
+      res.render('home',context);
+    });
+  });
+});
+
+app.get('/create-tables',function(req,res,next){
+  var context = {};
+
+  mysql.pool.query("DROP TABLE IF EXISTS `Movie`", function(err){
+    var createString = "CREATE TABLE `Movie`("+
+    "`movie_id` INT AUTO_INCREMENT,"+
+    "`title` VARCHAR(255) NOT NULL,"+
+    "`duration` INT NOT NULL,"+
+    "`release_year` INT NOT NULL,"+
+    "PRIMARY KEY(`movie_id`));";
+    mysql.pool.query(createString, function(err){
+      mysql.pool.query("DROP TABLE IF EXISTS `Genre`", function(err){
+        var string2 = "CREATE TABLE `Genre`(" +
+        "`genre_id` INT AUTO_INCREMENT,"+
+        "`type` VARCHAR(255) NOT NULL UNIQUE,"+
+        "PRIMARY KEY(`genre_id`));";
+        mysql.pool.query(string2, function(err){
+          mysql.pool.query("DROP TABLE IF EXISTS `Production_Company", function(err){
+          var string3 = "CREATE TABLE `Production_Company`("+
+          "`production_co_id` INT AUTO_INCREMENT,"+
+          "`name` VARCHAR(255) NOT NULL UNIQUE,"+
+          "`ceo` VARCHAR(255),"+
+          "`headquarters` VARCHAR(255) NOT NULL,"+
+          "PRIMARY KEY(`production_co_id`));";
+          mysql.pool.query(string3, function(err){
+            mysql.pool.query("DROP TABLE IF EXISTS `Director`", function(err){
+              var string4 = "CREATE TABLE `Director`("+
+              "`director_id` INT AUTO_INCREMENT,"+
+              "`f_name` VARCHAR(255) NOT NULL,"+
+              "`l_name` VARCHAR(255) NOT NULL,"+
+              "`age` INT NOT NULL,"+
+              "PRIMARY KEY(`director_id`));";
+              mysql.pool.query(string4, function(err){
+                mysql.pool.query("DROP TABLE IF EXISTS `Sequels`", function(err){
+                  var string5 = "CREATE TABLE `Sequels`("+
+                  "`movie_id` INT,"+
+                  "`sequel_id` INT,"
+                  "FOREIGN KEY(`movie_id`)"+
+                  "  REFERENCES `Movie`(`movie_id`),"+
+                  "FOREIGN KEY(`sequel_id`)"+
+                  "  REFERENCES `Movie`(`movie_id`));";
+                  mysql.pool.query(string5, function(err){
+                    mysql.pool.query("DROP TABLE IF EXISTS `Movie_Production_Co`", function(err){
+                      var string6 = "CREATE TABLE `Movie_Production_Co`("+
+                      "`movie_id` INT,"+
+                      "`production_co_id` INT,"+
+                      "FOREIGN KEY(`movie_id`)"+
+                      "  REFERENCES `Movie`(`movie_id`),"+
+                      "FOREIGN KEY(`production_co_id`)"+
+                      "  REFERENCES `Production_Company`(`production_co_id`));";
+                      mysql.pool.query(string6, function(err){
+                        mysql.pool.query("DROP TABLE IF EXISTS `Movie_Director`", function(err){
+                          var string7 = "CREATE TABLE `Movie_Director`("+
+                          "`movie_id` INT,"+
+                          "`director_id` INT,"+
+                          "FOREIGN KEY(`movie_id`)"+
+                          "  REFERENCES `Movie`(`movie_id`),"+
+                          "FOREIGN KEY(`director_id`)"+
+                          "  REFERENCES `Director`(`director_id`));";
+                          mysql.pool.query(string7, function(err){
+                            mysql.pool.query("DROP TABLE IF EXISTS `Movie_Genre`", function(err){
+                              var string8 = "CREATE TABLE `Movie_Genre`("+
+                              "`movie_id` INT,"+
+                              "`genre_id` INT,"+
+                              "FOREIGN KEY(`movie_id`)"+
+                              "  REFERENCES `Movie`(`movie_id`),"+
+                              "FOREIGN KEY(`genre_id`)"+
+                              "  REFERENCES `Genre`(`genre_id`));";
+                              mysql.pool.query(string8, function(err){
+                                context.results = "Table reset";
+                                res.render('home',context);
+                              });
+                            });
+                          });
+                        });
+                      });
+                    });
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+});
+});    
+
+
+
 app.use(function(req,res){
   res.status(404);
   res.render('404');
